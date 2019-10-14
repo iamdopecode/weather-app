ccbf51c35e3f01689a3e8137c6b3b021bcf7a041
Shubham <shubham13202@yahoo.com>
2019-10-14 06:32:59 +0530

    Some changes made to weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -2,9 +2,9 @@
 
 import requests
 
-api_address='https://api.openweathermap.org/data/2.5/weather?appid=b6907d289e10d714a6e88b30761fae22&='
+api_address='https://api.openweathermap.org/data/2.5/weather?appid=20689b2be145969d78f3bf4ab0a9ff58&='
 city=input("Enter the City") # taking input from user
 api_url=api_address+city
-json_data=requests.get(url).json
+json_data=requests.get(formatted_url).json
 formatted_data=json_data['weather'][0]['main']
 print(formatted_data)
