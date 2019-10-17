03b6eeef2a185a064fec8527ab7ef1d473c2b8ca
Shubham <shubham13202@yahoo.com>
2019-10-17 15:09:05 +0530

    Modified the code

--- weather_app.py
+++ weather_app.py
@@ -1,10 +1,17 @@
 # importing modules
 
 import requests
+from pprint import pprint
 
-api_address='https://api.openweathermap.org/data/2.5/weather?appid=20689b2be145969d78f3bf4ab0a9ff58&='
-city=input("Enter the City") # taking input from user
-api_url=api_address+city
-json_data=requests.get(formatted_url).json
-formatted_data=json_data['weather'][0]['main']
-print(formatted_data)
+city=input("Enter the City: ")
+
+url='https://api.openweathermap.org/data/2.5/weather?appid=20689b2be145969d78f3bf4ab0a9ff58&q={}&units=metric'.format(city)
+
+
+
+res=requests.get(url)
+
+data=res.json()
+
+pprint(data)
+
