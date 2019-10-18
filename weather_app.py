14041e8d33ee4ae2efc9bd55d6c2518a62b2346a
Shubham <shubham13202@yahoo.com>
2019-10-18 20:59:33 +0530

    Modified the code

--- weather_app.py
+++ weather_app.py
@@ -2,23 +2,39 @@
 
 import requests
 
+def by_city():
+    city=input("Enter the City: ")
+    url='https://api.openweathermap.org/data/2.5/weather?appid=20689b2be145969d78f3bf4ab0a9ff58&q={}&units=metric'.format(city)
+    res=requests.get(url)
+    data=res.json()
+    show_data(data)
 
-city=input("Enter the City: ")
+def by_location():
+    res=requests.get('https://ipinfo.io') 
+    data=res.json()
+    show_data(data)
 
-url='https://api.openweathermap.org/data/2.5/weather?appid=20689b2be145969d78f3bf4ab0a9ff58&q={}&units=metric'.format(city)
+def show_data(data):
 
+    temp=data['main']['temp']
+    wind_speed=data['wind']['speed']
+    description=data['weather'][0]['description']
+    print("The Temperature is: {} degree celcius".format(temp))
+    print("The Wind Speed is: {} m/s".format(wind_speed))
+    print("The Description is: {}".format(description))
 
+def main_menu():
+    print('1. Get Data by city')
+    print('2. Get Data by current location')
+    choice=input("Enter your choice: ")
 
-res=requests.get(url)
+    if choice=='1':
+        by_city()
+    else:
+        by_location()
 
-data=res.json()
+main_menu()
 
-temp=data['main']['temp']
-
-wind_speed=data['wind']['speed']
-
-description=data['weather'][0]['description']
-print("The Temperature is: {} degree celcius".format(temp))
-print("The Wind Speed is: {} m/s".format(wind_speed))
-print("The Description is: {}".format(description))
+        
+    
 
